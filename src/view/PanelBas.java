1573cc3e557f1715ca552aeffcd23d5e632a9ae5
Damien Level <level.damien@gmail.com>
2012-03-27 15:52:54 +0200

    PanelBas

--- src/view/PanelBas.java
+++ src/view/PanelBas.java
@@ -1,12 +1,14 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class PanelBas extends JPanel {
 	
@@ -14,12 +16,28 @@ public class PanelBas extends JPanel {
 		this.setPreferredSize(new Dimension(800, 200));
 		this.setMinimumSize(new Dimension(800, 200));
 		this.setMaximumSize(new Dimension(800, 200));
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Ajout"),BorderFactory.createEmptyBorder(10,10,10,10)));
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Modification"),BorderFactory.createEmptyBorder(10,20,10,10)));
 
-		JButton buttonAdd = new JButton("Ajouter");
+		JButton buttonAdd = new JButton("Ajouter une couleur");
+		buttonAdd.addActionListener(new AddLignePanel());
 		this.add(buttonAdd);
+		this.add(Box.createRigidArea(new Dimension(0,10)));
+		JButton buttonModifAuto = new JButton("Modification Automatique");
+		buttonModifAuto.addActionListener(new ModifAuto());
+		this.add(buttonModifAuto);
 		
 	}
 
+	class AddLignePanel extends AbstractAction{	
+		public void actionPerformed(ActionEvent arg0) {
+			
+		}
+	}
+	
+	class ModifAuto extends AbstractAction{	
+		public void actionPerformed(ActionEvent arg0) {
+			
+		}
+	}
 }
