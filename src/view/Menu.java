3e477a4737e667698623c768e1677381cfad4a08
Damien Level <level.damien@gmail.com>
2012-03-27 14:23:10 +0200

    Menu

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,5 +1,20 @@
 package view;
 
-public class Menu {
 
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Menu extends JMenuBar{
+	public Menu(){
+		JMenu menu = new JMenu("File");
+		this.add(menu);
+		menu = new JMenu("Edit");
+		this.add(menu);
+		JMenuItem menuItem = new JMenuItem("Undo");
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Redo");
+		menu.add(menuItem);
+	}
+	
 }
